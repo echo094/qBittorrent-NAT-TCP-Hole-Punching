45a28d8a0a869d7b377209e61f9e7266926d31d4
echo094 <20028238+echo094@users.noreply.github.com>
2023-04-08 16:57:24 +0800

    add transmission rpc
    Signed-off-by: echo094 <20028238+echo094@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -1,19 +1,39 @@
 #!/bin/sh
 
 # Natter/NATMap
-private_port=$4 # Natter: $3; NATMap: $4
-public_port=$2 # Natter: $5; NATMap: $2
+tool_type="NATMap"
+# qBittorrent/transmission
+downloader_type="qBittorrent"
+host="localhost"
+web_port="8080"
+username="admin"
+password="adminadmin"
 
-# qBittorrent.
-qb_web_port="8080"
-qb_username="admin"
-qb_password="adminadmin"
+if [[ "$tool_type" == "NATMap" ]]; then
+public_ip=$1
+public_port=$2
+private_port=$4
+protocol=$5
+else
+protocol=$1
+private_port=$3
+public_ip=$4
+public_port=$5
+fi
+echo "Mapping $protocol://$public_ip:$public_port -> $private_port"
 
+if [[ "$downloader_type" == "transmission" ]]; then
+echo "Update transmission listen port to $public_port..."
+# Update transmission listen port.
+tr_session=$(curl --silent -u "$username:$password" "http://$host:$web_port/transmission/web" | grep --regexp="X-Transmission-Session-Id: [0-9a-zA-Z]*" -o)
+echo $(curl --silent "http://$host:$web_port/transmission/rpc" -u "$username:$password" -H "${tr_session}" --data-raw '{"method":"session-set","arguments":{"peer-port":'$public_port',"_easyui_textbox_input59":'$public_port'},"tag":""}')
+echo $(curl --silent "http://$host:$web_port/transmission/rpc" -u "$username:$password" -H "${tr_session}" --data-raw '{"method":"session-get","arguments":{"fields": ["peer-port"]},"tag":""}')
+else
 echo "Update qBittorrent listen port to $public_port..."
-
 # Update qBittorrent listen port.
-qb_cookie=$(curl -s -i --header "Referer: http://localhost:$qb_web_port" --data "username=$qb_username&password=$qb_password" http://localhost:$qb_web_port/api/v2/auth/login | grep -i set-cookie | cut -c13-48)
-curl -X POST -b "$qb_cookie" -d 'json={"listen_port":"'$public_port'"}' "http://localhost:$qb_web_port/api/v2/app/setPreferences"
+qb_cookie=$(curl -s -i --header "Referer: http://$host:$web_port" --data "username=$username&password=$password" http://$host:$web_port/api/v2/auth/login | grep -i set-cookie | cut -c13-48)
+curl -X POST -b "$qb_cookie" -d 'json={"listen_port":"'$public_port'"}' "http://$host:$web_port/api/v2/app/setPreferences"
+fi
 
 echo "Update iptables..."
 
